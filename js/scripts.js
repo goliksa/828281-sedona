8a8f571df4cb1f12866405c2717f4d5ffbd23483
Sergey Golik <goliksa@gmail.com>
2018-09-06 11:48:44 +0300

    Оживил появление-скрытие формы

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".interesting button");
+var form = document.querySelector(".search-form");
+
+button.addEventListener("click", function() {
+    form.classList.toggle("o_O-where-is-it");
+})
